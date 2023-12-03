de5f2c58c4eeb9872c6c1fd105b51d596f228718
John Z. Li <lizhao.johnlee@gmail.com>
2023-12-03 12:26:44 +0800

    add script to update command line tools

---
+++ update_all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# update fzf
+pushd /home/johnzli/home_local/src/fzf
+git pull &&  ./install -all
+popd
+
+# upodate Rust tools
+cargo install-update -a
+
+# update JS tools
+npm outdated |npm update
+rm package-lock.json
+
+# update ctags
+pushd /home/johnzli/home_local/src/ctags
+git pull
+./autogen.sh
+./configure --prefix=/home/johnzli/home_local/
+make && make install
+
